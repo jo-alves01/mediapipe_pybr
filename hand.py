9c2bed7b0be73b378fe14c4a07acb84e69e2c2b0
Josué Alves <josuealves01@hotmail.com>
2022-10-17 01:10:34 -0400

    first commit

---
+++ hand.py
@@ -0,0 +1,62 @@
+import cv2
+import mediapipe as mp
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=1, modelComplexity=1, detectionCon=0.5, trackCon=0.5):#constructor
+        self.mode=mode 
+        self.maxHands=maxHands
+        self.modelComplex = modelComplexity
+        self.detectionCon=detectionCon
+        self.trackCon=trackCon
+        self.mpHands=mp.solutions.hands#initializing hands module for the instance
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex, self.detectionCon, self.trackCon) #object for Hands for a particular instance
+        self.mpDraw=mp.solutions.drawing_utils#object for Drawing
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    def findHands(self,img,draw=True):
+        imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)#converting to RGB bcoz hand recognition works only on RGB image
+        self.results=self.hands.process(imgRGB)#processing the RGB image 
+        if self.results.multi_hand_landmarks:# gives x,y,z of every landmark or if no hand than NONE
+            for handLms in self.results.multi_hand_landmarks:#each hand landmarks in results
+                if draw:
+                     self.mpDraw.draw_landmarks(img,handLms,self.mpHands.HAND_CONNECTIONS)#joining points on our hand
+        
+        return img
+
+    def findPosition(self,img,handNo=0,draw=True):
+        xList=[]
+        yList=[]
+        bbox=[]
+        self.lmlist=[]
+        if self.results.multi_hand_landmarks:# gives x,y,z of every landmark    
+            myHand=self.results.multi_hand_landmarks[handNo]#Gives result for particular hand 
+            for id,lm in enumerate(myHand.landmark):#gives id and lm(x,y,z)
+                h,w,c=img.shape#getting h,w for converting decimals x,y into pixels 
+                cx,cy=int(lm.x*w),int(lm.y*h)# pixels coordinates for landmarks
+                xList.append(cx)
+                yList.append(cy)
+                self.lmlist.append([id,cx,cy])
+                if draw:
+                    cv2.circle(img,(cx,cy),5,(255,255,255),cv2.FILLED)    
+            xmin,xmax=min(xList),max(xList)
+            ymin,ymax=min(yList),max(yList)
+            bbox=xmin,ymin,xmax,ymax
+
+        return self.lmlist,bbox
+
+    def fingersUp(self):#checking which finger is open 
+        fingers = []#storing final result
+        # Thumb < sign only when  we use flip function to avoid mirror inversion else > sign
+        if self.lmlist[self.tipIds[0]][1] > self.lmlist[self.tipIds[0] - 1][1]:#checking x position of 4 is in right to x position of 3
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Fingers
+        for id in range(1, 5):#checking tip point is below tippoint-2 (only in Y direction)
+            if self.lmlist[self.tipIds[id]][2] < self.lmlist[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        return fingers
---
+++ mediapipe_drawing.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+import hand 
+
+drawColor=(255,255,255)#setting white color
+brushThickness = 3
+eraserThickness = 30
+
+xp, yp = 0, 0
+imgCanvas = np.zeros((550, 550, 3), np.uint8)# defining canvas
+
+detector = hand.handDetector(detectionCon=0.8)#making object
+cap=cv2.VideoCapture(1)
+
+while True:
+
+    # 1. Import image
+    success, img = cap.read()
+    img=cv2.flip(img,1)#for neglecting mirror inversion
+    
+    # 2. Find Hand Landmarks
+    img = detector.findHands(img)#using functions fo connecting landmarks
+    lmList,bbox = detector.findPosition(img, draw=False)#using function to find specific landmark position,draw false means no circles on landmarks
+    
+    if len(lmList)!=0:
+        #print(lmList)
+        x1, y1 = lmList[8][1],lmList[8][2]# tip of index finger
+        x2, y2 = lmList[12][1],lmList[12][2]# tip of middle finger
+        
+        # 3. Check which fingers are up
+        fingers = detector.fingersUp()
+
+        if fingers[1] and fingers[2] == False:
+            drawColor = (255, 255, 255)
+            cv2.circle(img, (x1, y1), 10, drawColor, cv2.FILLED)#drawing mode is represented as circle
+            if xp == 0 and yp == 0:#initially xp and yp will be at 0,0 so it will draw a line from 0,0 to whichever point our tip is at
+                xp, yp = x1, y1 # so to avoid that we set xp=x1 and yp=y1
+            #till now we are creating our drawing but it gets removed as everytime our frames are updating so we have to define our canvas where we can draw and show also
+            
+            cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+            xp,yp=x1,y1 # giving values to xp,yp everytime
+
+        if fingers[1] and fingers[2] == True:
+            drawColor = (0, 0, 0)
+            cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
+            xp,yp=x1,y1
+
+    cv2.imshow("Canvas", imgCanvas)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    cv2.imshow('Raw Webcam Feed', image)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
